88c9d934eec06535f542f91a56931730cce00c9d
Thomas Kemper <thomas.kemper@epfl.ch>
2025-12-21 00:05:18 +0100

    feat: create index and viz pages with enhanced layout and styling

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
-export default defineConfig({
-  base: "/ada-2025-project-radatouille/", // <-- repo name, with slashes
+export default defineConfig(({ command }) => ({
+  // dev: serve at /
+  // build (GitHub Pages): serve under /ada-2025-project-radatouille/
+  base: command === "serve" ? "/" : "/ada-2025-project-radatouille/",
   plugins: [react()],
-});
+  build: {
+    rollupOptions: {
+      input: {
+        intro: resolve(__dirname, "index.html"),
+        community: resolve(__dirname, "/community/index.html"),
+      },
+    },
+  },
+}));
